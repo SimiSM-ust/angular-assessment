33d7aad5a02159209da7aa2b88cd0c39361b7c0d
SimiSM-ust <138108544+SimiSM-ust@users.noreply.github.com>
2023-06-30 10:13:55 +0530

    4 files modified

--- src/app/exercises/exercise1/exercise1.component.ts
+++ src/app/exercises/exercise1/exercise1.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-exercise1',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./exercise1.component.css']
 })
 export class Exercise1Component {
+  @ViewChild('f',{ static: false })
+signupForm!: NgForm;
+
+  onSubmit() {
+    // this.submitted = true;
+    // this.user.username = this.signupForm.value.userData.username;
+    // this.user.email = this.signupForm.value.userData.email;
+    // this.user.secretQuestion = this.signupForm.value.secret;
+    // this.user.answer = this.signupForm.value.questionAnswer;
+    // this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
 
 }
--- src/app/exercises/exercise1/exercise1.module.ts
+++ src/app/exercises/exercise1/exercise1.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Exercise1Component } from './exercise1.component';
 import {RouterModule} from "@angular/router";
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,6 +12,7 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([{
       path: '',
       component: Exercise1Component
